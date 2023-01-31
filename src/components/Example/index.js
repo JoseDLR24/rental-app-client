240c4704996aeb75a0f3f7956fd89e5c112155c7
Andres Correa <andrescorrea.m@hotmail.com>
2023-01-31 13:10:16 -0500

    Setting connection with the backend GETALLDATA

--- src/components/Example/index.js
+++ src/components/Example/index.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 
 const Example = () => {
+  const [getFullData, setGetFullData] = useState([]);
+
+  useEffect(() => {
+    fetch("/data")
+      .then((response) => response.json())
+      .then((data) => setGetFullData(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  console.log(getFullData);
+
   return (
     <div className="example-container">
-      <h1>THIS IS AN EXAMPLE FROM FOLDER EXAMPLE</h1>
+      <h1>GETTING DATA FROM THE API</h1>
+      {getFullData.length === 0 ? (
+        <h2>LOADING ....</h2>
+      ) : (
+        getFullData.map((data) => {
+          return (
+            <ul key={data.name}>
+              <li>Name: {data.name}</li>
+              <li>Last Name: {data.lastname}</li>
+              <li>Age: {data.age}</li>
+            </ul>
+          );
+        })
+      )}
+      ;
     </div>
   );
 };

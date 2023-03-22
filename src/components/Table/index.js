973c6449b8b940e7a9e50a3d22498f625893c8e4
Jose De Los Reyes <jddelosreyes23@hotmail.com>
2023-03-21 22:50:09 -0400

    fixed table added

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -6,54 +6,76 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 const Table = () => {
     return(
         <main class="scrappertable-container">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Resources</th>
-                        <th>Picture</th>
-                        <th>Location</th>
-                        <th>Type of house</th>
-                        <th>Number of Rooms</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Kijiji.ca</td>
-                        <td>picture</td>
-                        <td>Barrie</td>
-                        <td>Apartment</td>
-                        <td>2 Bedrooms</td>
-                    </tr>
-                    <tr>
-                        <td>Kijiji.ca</td>
-                        <td>picture</td>
-                        <td>Barrie</td>
-                        <td>Apartment</td>
-                        <td>2 Bedrooms</td>
-                    </tr>
-                    <tr>
-                        <td>Kijiji.ca</td>
-                        <td>picture</td>
-                        <td>Barrie</td>
-                        <td>Apartment</td>
-                        <td>2 Bedrooms</td>
-                    </tr>
-                    <tr>
-                        <td>Kijiji.ca</td>
-                        <td>picture</td>
-                        <td>Barrie</td>
-                        <td>Apartment</td>
-                        <td>2 Bedrooms</td>
-                    </tr>
-                    <tr>
-                        <td>Kijiji.ca</td>
-                        <td>picture</td>
-                        <td>Barrie</td>
-                        <td>Apartment</td>
-                        <td>2 Bedrooms</td>
-                    </tr>
-                </tbody>
-            </table>
+            <div class="table-wrapper">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Date Collected</th>
+                            <th>Location</th>
+                            <th>Local Municipality</th>
+                            <th>Housing Type</th>
+                            <th>Stratified Area</th>
+                            <th>Street No.</th>
+                            <th>Street Name</th>
+                            <th>Street Type</th>
+                            <th>Postal Code</th>
+                            <th>Unit Size</th>
+                            <th>Secondary Suite</th>
+                            <th>Monthly Rent</th>
+                            <th>Utilities Included</th>
+                            <th>Add Hydro</th>
+                            <th>Add Gas</th>
+                            <th>Lease</th>
+                            <th>Possible duplicate</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Kijiji.ca</td>
+                            <td>picture</td>
+                            <td>Barrie</td>
+                            <td>Apartment</td>
+                            <td>2 Bedrooms</td>
+                        </tr>
+                        <tr>
+                            <td>Kijiji.ca</td>
+                            <td>picture</td>
+                            <td>Barrie</td>
+                            <td>Apartment</td>
+                            <td>2 Bedrooms</td>
+                        </tr>
+                        <tr>
+                            <td>Kijiji.ca</td>
+                            <td>picture</td>
+                            <td>Barrie</td>
+                            <td>Apartment</td>
+                            <td>2 Bedrooms</td>
+                        </tr>
+                        <tr>
+                            <td>Kijiji.ca</td>
+                            <td>picture</td>
+                            <td>Barrie</td>
+                            <td>Apartment</td>
+                            <td>2 Bedrooms</td>
+                        </tr>
+                        <tr>
+                            <td>Kijiji.ca</td>
+                            <td>picture</td>
+                            <td>Barrie</td>
+                            <td>Apartment</td>
+                            <td>2 Bedrooms</td>
+                        </tr>
+                        <tr>
+                            <td>Kijiji.ca</td>
+                            <td>picture</td>
+                            <td>Barrie</td>
+                            <td>Apartment</td>
+                            <td>2 Bedrooms</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            
         </main>
     );
 };

f5942f4d82c1fc55a5e83f0418ad6bb014c88e43
Marcello Bernardes <mnunesbernardes@gmail.com>
2023-04-17 18:48:59 -0400

    changes to home css

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,27 +1,13 @@
 import React from "react";
 import aboveTheFold from './images/white.png';
 import "./style.css";
-import bgImage from './images/home-bg.jpeg';
+import bgImage from './images/home-bg.jpg';
 import { Link } from 'react-router-dom';
-
 
 const Main = () => {
   return (
     <section className="above-the-fold container-fluid">
-      <div className="AtF-bg" style={{
-        backgroundImage: `url(${bgImage})`,
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center center",
-        position: "absolute",
-        top: "0",
-        left: "0",
-        right: "0",
-        bottom: "0",
-        zIndex: "-1",
-        opacity: "0.9",
-        filter: "blur(2px)"
-      }}></div>
+      <div className="AtF-bg"  style={{backgroundImage: `url(${bgImage})`}}></div>
       <div className="AtF-content">
         <img src={aboveTheFold} alt="CCSI logo and Slogan Affordable Housing lab" />
         <h1>
